42b1cb07d93e81e2221623534e53360ffc4ece40
o_o <smithtrombone@gmail.com>
2019-02-04 21:11:56 -0500

    moved smart_dev member function definitions from shi.cpp to func.cpp

--- func.cpp
+++ func.cpp
@@ -1,4 +1,62 @@
 #include "shi.h"
+//member functions for smart_dev class
+smart_dev::smart_dev ()
+{
+}
+
+smart_dev::~smart_dev ()
+{
+}
+
+void smart_dev::add_event (std::string name)
+{
+	event_types ++;
+	events.push_back (event (name));
+}
+
+int smart_dev::get_event_types ()
+{
+	return event_types;
+}
+
+void smart_dev::remove_event (int event_type)
+{
+	events.erase (events.begin () + event_type);
+}
+
+void smart_dev::remove_event (std::string name)
+{
+	std::vector<event>::iterator it = events.begin ();
+	while (it->event_name != name)
+		it ++;
+	events.erase (it);
+}
+
+bool smart_dev::is_active (int event_type)
+{
+	return events.at (event_type).event_active;
+}
+
+bool smart_dev::is_active (std::string name)
+{
+	std::vector<event>::iterator it = events.begin ();
+	while (it->event_name != name)
+		it ++;
+	return it->event_active;
+}
+
+float smart_dev::get_event_value (int event_type)
+{
+	return events.at (event_type).event_value;
+}
+
+float smart_dev::get_event_value (std::string name)
+{
+	std::vector<event>::iterator it = events.begin ();
+	while (it->event_name != name)
+		it ++;
+	return it->event_value;
+}
 
 //Backend functions are defined in this file
 
